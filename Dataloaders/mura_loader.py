68cd4c7fbdec4612961ef909b05e6b59d28b9f8f
mrcaelumn <caelumnproperty@gmail.com>
2022-06-22 00:24:04 +0800

    add mura dataset

---
+++ Dataloaders/mura_loader.py
@@ -0,0 +1,100 @@
+from PIL import Image
+import numpy as np
+import torch.utils.data
+from torchvision import datasets, transforms
+import torchvision
+import cv2
+import os
+import itertools
+import random
+
+
+def download_class_mura(opt):
+    opt.input_name = "mura_train_numImages_" + str(opt.num_images) + "_" + str(opt.policy) + "_" + str(opt.pos_class) \
+                     + "_indexdown" + str(opt.index_download) + ".png"
+    class_to_idx = {
+        "normal": 0,
+        "defect": 1
+    }
+    scale = opt.size_image
+    pos_class = opt.pos_class
+    num_images = opt.num_images
+
+    def imsave(img, i):
+        transform = transforms.Compose([transforms.ToPILImage(), transforms.Resize((scale, scale))])
+        im = transform(img)
+        im.save("Input/Images/mura_train_numImages_" + str(opt.num_images) +"_" + str(opt.policy) + "_" + str(pos_class)
+                    + "_indexdown" +str(opt.index_download) + "_" + str(i) + ".png")
+
+    def load_dataset_from_folder(path, c_transforms=None, bs=None, shuf=True):
+        dataset = datasets.ImageFolder(
+            root='Data/mura_march_clean/test_data'
+            , transform=transforms.ToTensor()
+            # , class_to_idx=class_to_idx
+        )
+        # print(dataset.classes)
+        # print(dataset.class_to_idx)
+        trainset = torch.utils.data.DataLoader(dataset, batch_size=bs, shuffle=shuf)
+        
+        trainset_data = []
+        trainset_targets = []
+        
+        for images, labels in trainset:
+            trainset_data.append(images)
+            trainset_targets.append(labels)
+            # print(images, labels)
+    
+        
+        train_data = np.array(trainset_data)
+        train_labels = np.array(trainset_targets)
+        
+        return train_data, train_labels
+    
+    if opt.mode == "train":
+        
+        # trainset = datasets.MNIST('./mnist_data', download=True, train=True)
+        
+        train_data, train_labels = load_dataset_from_folder('Data/mura_march_clean/train_data', c_transforms=transforms.ToTensor())
+        
+        train_data = train_data[np.where(train_labels == int(pos_class))]
+
+        dicty = {}
+        
+        random_index = random.sample(range(0, len(train_data)), opt.num_images)
+        training_images = list(random_index)
+        for i in range(len(training_images)):
+            index = training_images[i]
+            t = train_data[index]
+            imsave(t, i)
+            
+        print("training images: ", training_images)
+
+        genertator0 = itertools.product((0,), (False, True), (-1, 1, 0), (-1,), (0,))
+        genertator1 = itertools.product((0,), (False, True), (0, 1), (0, 1), (0, 1, 2, 3))
+        genertator3 = itertools.product((0,), (False, True), (-1,), (1, 0), (0,))
+        genertator = itertools.chain(genertator0, genertator1, genertator3)
+        lst = list(genertator)
+        random.shuffle(lst)
+        path_transform = "TrainedModels/" + str(opt.input_name)[:-4]
+        if os.path.exists(path_transform) == False:
+            os.mkdir(path_transform)
+        np.save(path_transform +  "/transformations.npy", lst)
+
+    path = "mura_test_scale" + str(scale) + "_" + str(pos_class) + "_" + str(num_images)
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+        
+    # mura_testset = datasets.MNIST('./mnist_data', download=True, train=False)
+    
+    
+#     test_data = np.array(mura_testset.data)
+#     test_labels = np.array(mura_testset.targets)
+    
+    test_data, test_labels = load_dataset_from_folder('Data/mura_march_clean/test_data', c_transforms=transforms.ToTensor())
+    
+    np.save(path + "/mura_data_test_" + str(pos_class) + str(scale) +  "_" + str(opt.index_download) + ".npy", test_data)
+    np.save(path + "/mura_labels_test_" + str(pos_class) + str(scale) +  "_" + str(opt.index_download) + ".npy", test_labels)
+
+    opt.input_name = "mura_train_numImages_" + str(opt.num_images) + "_" + str(opt.policy) + "_" + str(pos_class) \
+                     + "_indexdown" + str(opt.index_download) + ".png"
+    return opt.input_name
--- main_train.py
+++ main_train.py
@@ -7,6 +7,7 @@ from Dataloaders.mnist_loader import download_class_mnist
 from Dataloaders.fashionmnist_loader import download_class_FashionMnist
 from Dataloaders.paris_loader import download_class_paris
 from Dataloaders.mvtec_loader import download_class_mvtec
+from Dataloaders.mura_loader import download_class_mura
 from anomaly_detection_evaluation import anomaly_detection
 from defect_detection_evaluation import defect_detection
 
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     parser = get_arguments()
     # parser.add_argument('--dataset', help='cifar/mnist/fashionmnist/mvtec/paris', default='mvtec')
     parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
-    # parser.add_argument('--pos_class', help='normal class', default='carpet')
+    parser.add_argument('--pos_class', help='normal class', default=0)
     parser.add_argument('--random_images_download', help='random selection of images', default=False)
     parser.add_argument('--num_images', type=int, help='number of images to train on', default=20)
     parser.add_argument('--if_download', type=bool, help='do you want to download class', default=False)
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('--niter_gray', help='number of iterations in each scale', type=int, default=500)
     parser.add_argument('--niter_rgb', help='number of iterations in each scale', type=int, default=1000)
     parser.add_argument('--index_download', help='index in dataset for starting download', type=int, default=1)
-    parser.add_argument('--use_internal_load', help='using another dataset', default=False)
+    # parser.add_argument('--use_internal_load', help='using another dataset', default=False)
     parser.add_argument('--experiment', help='task to be done', default='stop_signs')
     parser.add_argument('--test_size', help='test size', type=int, default=10000)
     parser.add_argument('--num_transforms', help='54 for rgb, 42 for grayscale', type=int, default=54)
@@ -43,7 +44,7 @@ if __name__ == '__main__':
     opt.niter = 54
     opt.niter_rgb
     opt.size_image = 128
-    opt.input_dir = 
+    # opt.input_dir = 
     # if opt.if_download == True:
     #     if dataset == 'cifar':
     #         opt.num_transforms, opt.niter = 54, opt.niter_rgb
@@ -60,13 +61,16 @@ if __name__ == '__main__':
     #     elif dataset == 'mvtec':
     #         opt.num_transforms, opt.niter = 54, opt.niter_rgb
     #         opt.input_name = download_class_mvtec(opt)
-
-
+    
+    opt.num_transforms, opt.niter = 54, opt.niter_rgb
+    opt.input_name = download_class_mura(opt)
+    print(opt.input_name)
     opt = functions.post_config(opt)
     Gs = []
     Zs,NoiseAmp = {}, {}
     reals_list = torch.FloatTensor(num_images,1,int(opt.nc_im), int(opt.size_image), int(opt.size_image)).cuda()
 
+    
     for i in range(num_images):
         real = img.imread("%s/%s_%d.png" % (opt.input_dir, opt.input_name[:-4], i))
         # if dataset == 'mnist' or dataset == 'fashionmnist':
@@ -83,10 +87,10 @@ if __name__ == '__main__':
     lst =  np.load("TrainedModels/" + str(opt.input_name)[:-4] +  "/transformations.npy")
     opt.list_transformations = lst
     print(real.shape)
-#     if opt.mode == 'train':
+    if opt.mode == 'train':
         
-#         train(opt, Gs, Zs, reals, NoiseAmp)
-#     if dataset == 'mvtec':
-#         defect_detection(opt.input_name, opt.test_size, opt)
-#     else:
-#         anomaly_detection(opt.input_name, opt.test_size, opt)
+        train(opt, Gs, Zs, reals, NoiseAmp)
+    if dataset == 'mvtec':
+        defect_detection(opt.input_name, opt.test_size, opt)
+    else:
+        anomaly_detection(opt.input_name, opt.test_size, opt)
